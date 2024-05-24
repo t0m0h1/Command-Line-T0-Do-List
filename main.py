a6ef8addf082e10c2c2818d81190217fedd972a5
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-05-24 19:24:30 +0100

    task class and methods

---
+++ main.py
@@ -0,0 +1,15 @@
+class Task:
+    def __init__(self, name):
+        self.name = name
+
+    def set_title(self, title):
+        self.title = title
+    
+    def get_title(self):
+        return self.title
+    
+    def set_description(self, description):
+        self.description = description
+    
+    def get_description(self):
+        return self.description

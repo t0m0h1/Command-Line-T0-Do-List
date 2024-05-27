f72f8999b6d720a231a64d45524edf38b4c2a85b
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-05-27 13:22:58 +0100

    interface

---
+++ ui.py
@@ -0,0 +1,12 @@
+import tkinter as tk 
+
+class UI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("To-Do-List App")
+        self.geometry("600x600")
+        self.label = tk.Label(self, text="Hello, World!")
+        self.label.pack()
+
+    def run(self):
+        self.mainloop()
